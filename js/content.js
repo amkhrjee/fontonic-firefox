ccd65af443e15c60ec036e305d85f0cb84fb741b
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2024-02-21 04:42:59 +0530

    font changing works

---
+++ js/content.js
@@ -0,0 +1,69 @@
+let originalSerif, originalSansSerif, originalMonospace;
+const changeFontFamily = (node, serif, sansSerif, monospace) => {
+    if (node.nodeType === 1) {
+        const computedStyle = window.getComputedStyle(node);
+        const fontFamily = computedStyle.getPropertyValue("font-family");
+        if (fontFamily) {
+            if (fontFamily.includes("sans-serif")) {
+                if (sansSerif != "Default") {
+                    originalSansSerif = fontFamily;
+                    node.style.fontFamily = `'${sansSerif}', ${originalSansSerif}`;
+                } else {
+                    node.style.fontFamily = `${originalSansSerif}`;
+                }
+            } else if (fontFamily.includes("serif")) {
+                if (serif != "Default") {
+                    originalSerif = fontFamily;
+                    node.style.fontFamily = `'${serif}', ${originalSerif}`;
+                } else {
+                    node.style.fontFamily = `${originalSerif}`;
+                }
+            } else if (fontFamily.includes("monospace")) {
+                if (monospace != "Default") {
+                    originalMonospace = fontFamily;
+                    node.style.fontFamily = `'${monospace}', ${originalMonospace}`;
+                } else {
+                    node.style.fontFamily = `${originalMonospace}`;
+                }
+            }
+        }
+    }
+    // Recursively process child nodes
+    for (const childNode of node.childNodes) {
+        changeFontFamily(childNode, serif, sansSerif, monospace);
+    }
+};
+let message = {
+    action: "on-page-load",
+    domain: window.location.hostname,
+};
+// Tries to load font when page is loaded
+browser.runtime.sendMessage(message, undefined, (response) => {
+    if (response.type === "apply_font") {
+        console.log("Tryint to load fonts");
+        const serif = response.data.serif;
+        const sans_serif = response.data.sans_serif;
+        const monospace = response.data.monospace;
+        changeFontFamily(document.body, serif, sans_serif, monospace);
+    } else if (response.type === "none") {
+        console.log("Font not set for site");
+    }
+});
+// Listens for the popup buttons
+browser.runtime.onConnect.addListener((port) => {
+    port.onMessage.addListener((message) => {
+        {
+            if (message.type === "apply_font") {
+                console.log("Request received from popup for applying fonts");
+                const serif = message.data.serif;
+                const sans_serif = message.data.sans_serif;
+                const monospace = message.data.monospace;
+                changeFontFamily(document.body, serif, sans_serif, monospace);
+            } else if (message.type === "restore") {
+                location.reload();
+            } else if (message.type === "redirect") {
+                window.open(message.data.redirect_url, "_blank");
+            }
+        }
+    });
+});
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,40 @@
+// WARNING: TSC generated stuff don't touch
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator["throw"](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next(),
+            );
+        });
+    };
+
 // UI Elements
 const applyButton = document.querySelector(".apply");
 const supportPage = document.querySelector(".support-slide");
@@ -15,6 +52,7 @@ const fontSelectionForm = document.forms["fonts"];
 const serifSelect = fontSelectionForm.elements["serif"];
 const sansSerifSelect = fontSelectionForm.elements["sans_serif"];
 const monospaceSelect = fontSelectionForm.elements["monospace"];
+
 // Show Support Page
 let isSupportPageOpen = false;
 supportButton.addEventListener("click", () => {
@@ -30,8 +68,7 @@ supportButton.addEventListener("click", () => {
             supportPage.style.transform = "translateX(0)";
             isSupportPageOpen = !isSupportPageOpen;
         }, 100);
-    }
-    else {
+    } else {
         supportButtonIcon.innerHTML = "favorite";
         supportButtonText.innerHTML = "Sponsor";
         supportPage.style.transform = "translateX(18rem)";
@@ -43,4 +80,122 @@ supportButton.addEventListener("click", () => {
         }, 200);
         isSupportPageOpen = !isSupportPageOpen;
     }
+});
+
+// Handing main form
+fontSelectionForm.addEventListener("submit", (e) =>
+    __awaiter(this, void 0, void 0, function* () {
+        e.preventDefault();
+        const serifValue = serifSelect.value;
+        const sansSerifValue = sansSerifSelect.value;
+        const monospaceValue = monospaceSelect.value;
+        if (
+            !serifValue.length &&
+            !sansSerifValue.length &&
+            !monospaceValue.length
+        ) {
+            applyButton.innerHTML = "No Changes Made";
+            applyButton.style.color = "#ffb6ad";
+            setTimeout(() => {
+                applyButton.innerHTML = "Apply Selection";
+                applyButton.style.color = "#bccbaf";
+            }, 1000);
+        } else {
+            applyButton.innerHTML = "✔ Applied";
+            setTimeout(() => {
+                applyButton.innerHTML = "Apply Selection";
+            }, 2000);
+        }
+        console.log("MonoSpace Value", monospaceValue);
+        browser.tabs.query(
+            { active: true, lastFocusedWindow: true },
+            (tabs) => {
+                console.log("Popup.js -- tabs data", tabs);
+                if (tabs) {
+                    let message = {
+                        type: "apply_font",
+                        data: {
+                            serif: serifValue.length ? serifValue : "Default",
+                            sans_serif: sansSerifValue.length
+                                ? sansSerifValue
+                                : "Default",
+                            monospace: monospaceValue.length
+                                ? monospaceValue
+                                : "Default",
+                        },
+                    };
+                    const port = browser.tabs.connect(tabs[0].id);
+                    port.postMessage(message);
+                    // Saving in the local Storage
+                    const domain = new URL(tabs[0].url).hostname;
+                    const fontData = {
+                        serif: message.data.serif,
+                        sans_serif: message.data.sans_serif,
+                        monospace: message.data.monospace,
+                    };
+                    console.log(
+                        "Popup.js -- Saving font data into local Storage...",
+                    );
+                    if (
+                        serifValue.length ||
+                        sansSerifValue.length ||
+                        monospaceValue.length
+                    ) {
+                        control.style.display = "flex";
+                        browser.storage.local
+                            .set({ [domain]: fontData })
+                            .then(() => {
+                                console.log("Stored in local Storage!");
+                            });
+                    }
+                }
+            },
+        );
+    }),
+);
+
+paymentButtons[0].addEventListener("click", () => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        const port = chrome.tabs.connect(tabs[0].id);
+        port.postMessage({
+            type: "redirect",
+            data: {
+                redirect_url:
+                    "https://paypal.me/amkhrjee?country.x=IN&locale.x=en_GB",
+            },
+        });
+    });
+});
+paymentButtons[1].addEventListener("click", () => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        const port = chrome.tabs.connect(tabs[0].id);
+        port.postMessage({
+            type: "redirect",
+            data: {
+                redirect_url: "https://www.buymeacoffee.com/amkhrjee",
+            },
+        });
+    });
+});
+paymentButtons[2].addEventListener("click", () => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        const port = chrome.tabs.connect(tabs[0].id);
+        port.postMessage({
+            type: "redirect",
+            data: {
+                redirect_url: "https://twitter.com/amkhrjee",
+            },
+        });
+    });
+});
+paymentButtons[3].addEventListener("click", () => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        const port = chrome.tabs.connect(tabs[0].id);
+        port.postMessage({
+            type: "redirect",
+            data: {
+                redirect_url: "https://linktr.ee/amkhrjee",
+            },
+        });
+    });
 });
